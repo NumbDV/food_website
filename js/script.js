043d0f15a634c099fac064e568bb16e10b0395c3
Den <d3n.vor@gmail.com>
2022-05-12 13:12:22 +0300

    made simple slider

--- js/script.js
+++ js/script.js
@@ -205,28 +205,38 @@ window.addEventListener('DOMContentLoaded', () => {
 	// 	});
 	// });
 
-	getResource('http://localhost:3000/menu')
-		.then(data => createCard(data));
+	axios.get('http://localhost:3000/menu')
+		.then(data => {
+			data.data.forEach(({img, altimg, title, descr, price}) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
+		} );
 
-	function createCard(data) {
-		data.forEach(({img, altimg, title, descr, price}) => {
-			const element = document.createElement('div');
-			price *= 29.25;
-			element.classList.add('menu__item');
 
-			element.innerHTML = `
-				<img src=${img} alt=${altimg}>
-				<h3 class="menu__item-subtitle">${title}</h3>
-				<div class="menu__item-descr">${descr}}</div>
-				<div class="menu__item-divider"></div>
-				<div class="menu__item-price">
-					<div class="menu__item-cost">Цена:</div>
-					<div class="menu__item-total"><span>${price}</span> грн/день</div>
-				</div>
-			`;
-			document.querySelector('.menu . container').append(element);
-		});
-	}
+
+
+	// getResource('http://localhost:3000/menu')
+	// 	.then(data => createCard(data));
+
+	// function createCard(data) {
+	// 	data.forEach(({img, altimg, title, descr, price}) => {
+	// 		const element = document.createElement('div');
+	// 		price *= 29.25;
+	// 		element.classList.add('menu__item');
+
+	// 		element.innerHTML = `
+	// 			<img src=${img} alt=${altimg}>
+	// 			<h3 class="menu__item-subtitle">${title}</h3>
+	// 			<div class="menu__item-descr">${descr}}</div>
+	// 			<div class="menu__item-divider"></div>
+	// 			<div class="menu__item-price">
+	// 				<div class="menu__item-cost">Цена:</div>
+	// 				<div class="menu__item-total"><span>${price}</span> грн/день</div>
+	// 			</div>
+	// 		`;
+	// 		document.querySelector('.menu . container').append(element);
+	// 	});
+	// }
 
 	// new MenuCard(
 	// 	"img/tabs/vegy.jpg",
@@ -368,4 +378,47 @@ window.addEventListener('DOMContentLoaded', () => {
 	.then(data => data.json())
 	.then(res => console.log(res));
 
+	// Slider
+
+	const slides = document.querySelectorAll('.offer__slide'),
+		  prev = document.querySelector('.offer__slider-prev'),
+		  next = document.querySelector('.offer__slider-next'),
+		  total = document.querySelector('#total'),
+		  current = document.querySelector('#current');
+	let slideIndex = 1;
+
+	showSlides(slideIndex);
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+	} else {
+		total.textContent = slides.length;
+	}
+	function showSlides(n) {
+		if (n > slides.length) { // если слайд долистается до последнего, то возвращается к первому
+			slideIndex = 1;
+		}
+		if (n < 1) {
+			slideIndex = slides.length; // листаем от первого к последнему в обратную сторону
+		}
+
+		slides.forEach(item => item.style.display = 'none');
+		slides[slideIndex - 1].style.display = 'block';
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	}
+
+	function plusSlides(n) {
+		showSlides(slideIndex +=n);
+	}
+	prev.addEventListener('click', () => {
+		plusSlides(-1);
+	});
+	next.addEventListener('click', () => {
+		plusSlides(1);
+	});
 });
